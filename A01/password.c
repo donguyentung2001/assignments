f8aa5b2a793021c1ce14f2ee312109c534f5b33f
Tung Do <tdo@haverford.edu>
2022-01-26 19:58:16 -0500

    finish assignment 1.1

--- A01/password.c
+++ A01/password.c
@@ -1,3 +1,21 @@
+#include <stdio.h> 
+#include <string.h> 
+
 int main() {
-  return 0;
+	char input[100]; 
+	printf("Enter a word: \n"); 
+	scanf("%s", input);
+	for (int i = 0; i < strlen(input); i++) {
+		if (input[i] == 'e') { 
+			input[i] = '3'; 
+		}
+		if (input[i] == 'l') {
+			input[i] = '1'; 
+		} 
+		if (input[i] == 'a') {
+			input[i] = '@'; 
+		}	
+	}
+	printf("Your new password is %s \n", input); 
+	return 0;
 }
