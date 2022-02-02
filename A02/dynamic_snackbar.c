56b96f9020c6caeb0c04f79cee72586a15bf4148
Tung Do <tdo@haverford.edu>
2022-02-02 01:24:05 -0500

    dynamic snackbar done

--- A02/dynamic_snackbar.c
+++ A02/dynamic_snackbar.c
@@ -9,5 +9,36 @@ struct snack {
 };
 
 int main() {
+  char name[32]; 
+  float cost; 
+  int quantity; 
+  int count; 
+  printf("Enter a number of snacks: ");
+  scanf("%i", &count); 
+  struct snack* output = malloc(sizeof(struct snack)*count); 
+  if (output == NULL) { 
+    printf("Cannot allocate memory. Exiting"); 
+  }
+  else { 
+    for (int i = 0; i < count; i++) { 
+      struct snack current_snack;
+      printf("Enter a name: "); 
+      scanf("%s", name); 
+      printf("Enter a cost: ");
+      scanf("%f", &cost);  
+      printf("Enter a quantity: "); 
+      scanf("%i", &quantity); 
+      strcpy(current_snack.name,name); 
+      current_snack.cost = cost; 
+      current_snack.quantity = quantity; 
+      output[i] = current_snack; 
+    }
+    printf("Welcome to Tung's snack bar.\n"); 
+    for (int i = 0; i < count; i++) { 
+      printf("%i) %s cost: $%.2f quantity: %i \n", i, output[i].name, output[i].cost, output[i].quantity); 
+    }
+  }
+  free(output); 
+  output = NULL; 
   return 0;
 }
