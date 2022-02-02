a8693b81c8dd882b4ece427afebfa523f20fd5f8
Tung Do <tdo@haverford.edu>
2022-02-02 01:03:37 -0500

    repeat.c finished

--- A02/repeat.c
+++ A02/repeat.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
 
 int main() {
+  char input[32]; 
+  int count; 
+  printf("Enter a word: "); 
+  scanf("%s", input); 
+  printf("Enter a count: "); 
+  scanf("%i", &count); 
+  char* output = malloc(sizeof(char)*strlen(input)*count); 
+  if (output == NULL) { 
+    printf("Cannot allocate new string. Exiting... \n"); 
+  }
+  else { 
+    for (int i =0; i < count; i++) { 
+      for (int j=0; j < strlen(input); j++) { 
+        output[i*strlen(input)+j] = input[j]; 
+      }
+    }
+  }
+  printf("%s", output); 
+
+  free(output); 
+  output = NULL; 
+
   return 0;
 }
--- Vim-Workshop/vim-practice.c
+++ Vim-Workshop/vim-practice.c
@@ -1,24 +1,24 @@
+
+
 /*
- * Put your name and motto here
- */
+ *ateryour name and motto here
 include <stdio.h>
 
 int main() {
-printf("Hello vim")
-int a = 0
-scanf("Enter a number: ");
-if (a < 0) {
-  print("a less than zero");
-} else if (a > 0) {
-  print("a greater than zero");
-} else {
-  print(you win!)
+  printf("Hello vim")
+  int a = 0
+  scanf("Enter a number: ");
+  if (a < 0) {
+    print("a less than zero");
+} 
+  else if (a > 0) {
+    print("a greater than zero");
+} 
+  else {
+    print("you win!")
 }
   
 }
-}
-
-
 
 
 
