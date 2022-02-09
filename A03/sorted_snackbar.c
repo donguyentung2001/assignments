a3e83f893efd4101d47e21a1ff33ac7beabd3a99
Tung Do <tdo@haverford.edu>
2022-02-08 23:07:16 -0500

    done 2

--- A03/sorted_snackbar.c
+++ A03/sorted_snackbar.c
@@ -2,39 +2,87 @@
 // sorted_snackbar.c 
 // CS223 - Spring 2022
 // Ask the user for a list of snacks and store them in alphabetical order
-// Name:
-//
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 struct snack {
   char name[32];
-  int quantity;
   float cost;
-  struct snack* next;
+  int quantity;
 };
 
-// Insert a new node to a list (implemented as a linked list). 
-// The new node should store the given properties
-// Param snacks: the first item in the list (NULL if empty)
-// Param name: the snack name (max length is 32 characters)
-// Param quantity: the snack quantity
-// Param cost: the snack cost
-// Returns the first item in the list
-struct snack* insert_sorted(struct snack* snacks, 
-  const char* name, int quantity, float cost) {
+struct node { 
+  struct snack val; 
+  struct node* next;   
+};
 
-  // todo
-  return NULL;
+struct node* insert_sorted(char name[32], float cost, int quantity, struct node* head) { 
+  int count = 0;
+  struct node* before = NULL; 
+  struct snack val; 
+  strcpy(val.name,name); 
+  val.cost = cost; 
+  val.quantity = quantity; 
+  struct node* n = malloc(sizeof(struct node));
+  if (n == NULL) { 
+    printf("Error: Out of space \n"); 
+    exit(1); 
+  }
+  n->val = val; 
+  for (struct node* cur = head; cur != NULL;  cur = cur->next) { 
+    if (strcmp(((cur->val).name), name) < 0) { 
+      before = cur; 
+    }
+  }
+  if (before == NULL) { 
+    n->next = head; 
+    return n; 
+  }
+  else { 
+    struct node* old_next = before->next; 
+    before->next = n; 
+    n->next = old_next; 
+    return head; 
+  }
 }
 
-// Delete (e.g. free) all nodes in the given list of snacks
-// Param snacks: the first node in the list (NULL if empty)
-void clear(struct snack* snacks) {
+void print(struct node* list) { 
+  int count = 0; 
+  for (struct node* n = list; n!= NULL; n = n->next) {
+    printf("%i) %s cost: $%.2f quantity: %i \n", count, (n->val).name, (n->val).cost, (n->val).quantity); 
+    count = count + 1; 
+  }
 }
 
+void clear(struct node* list) {  
+  struct node* curr = list; 
+  while (curr != NULL) { 
+    struct node* next = curr->next; 
+    free(curr); 
+    curr = next;
+  }
+}
 int main() {
+  char name[32]; 
+  float cost; 
+  int quantity; 
+  int count; 
+  struct node* head = NULL; 
+  printf("Enter a number of snacks: ");
+  scanf("%i", &count); 
+  for (int i = 0; i < count; i++) { 
+    struct snack current_snack;
+    printf("Enter a name: "); 
+    scanf("%s", name); 
+    printf("Enter a cost: ");
+    scanf("%f", &cost);  
+    printf("Enter a quantity: "); 
+    scanf("%i", &quantity); 
+    head = insert_sorted(name, cost, quantity, head); 
+  }
+  printf("Welcome to Tung's snack bar.\n"); 
+  print(head); 
+  clear(head); 
   return 0;
 }
-
