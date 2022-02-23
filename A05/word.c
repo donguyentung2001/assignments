c6027b65e16527f055f54802dc750249a2cee027
Tung Do <tdo@haverford.edu>
2022-02-22 21:30:34 -0500

    testing 1

--- A05/word.c
+++ A05/word.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 int main() {
+  char input[4]; 
+  printf("Enter 4 characters: "); 
+  scanf(" %s", input); 
+  unsigned int* number_pt = (unsigned int*) &input; 
+  unsigned int number = *(number_pt);
+  printf("Your number is %i.\n", number); 
   return 0;
 }
