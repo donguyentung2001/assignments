b8c7005d42425dadaaa10daa2c3587924ef96106
Tung Do <tdo@haverford.edu>
2022-02-22 22:51:13 -0500

    done 2

--- A05/binarychar.c
+++ A05/binarychar.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
 int main() {
+  unsigned char input[9]; 
+  int number = 0; 
+  int add; 
+  printf("Enter 8 bits: "); 
+  scanf(" %s", input); 
+
+  for (int i = 0; i < 8; i++) {
+    if (input[i] == '1') { 
+      add = 1; 
+      add = add << (7-i); 
+      number += add;
+    }
+  } 
+
+  char* p = (char *) &number; 
+
+  printf("Your character is: %c \n", *p); 
+
   return 0;
 }
--- A05/word.c
+++ A05/word.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 int main() {
-  char input[4]; 
+  unsigned char input[4]; 
   printf("Enter 4 characters: "); 
   scanf(" %s", input); 
-  unsigned int* number_pt = (unsigned int*) &input; 
+  unsigned int* number_pt = (unsigned int*) &(input); 
   unsigned int number = *(number_pt);
-  printf("Your number is %i.\n", number); 
+  for (int i = 3; i >= 0; i--) { 
+    printf("Debug: %c = 0x%08X \n", input[i], input[i] << i*8);
+  }
+  printf("Your number is %i (0x%08X).\n", number, number); 
   return 0;
 }
