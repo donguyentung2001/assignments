4dab4e34a62113e718ecb64ea94812e471b749ae
Tung Do <tdo@haverford.edu>
2022-02-23 14:45:51 -0500

    fix 1

--- A05/word.c
+++ A05/word.c
@@ -2,12 +2,14 @@
 
 int main() {
   unsigned char input[4]; 
+  unsigned int current; 
+  unsigned int number = 0; 
   printf("Enter 4 characters: "); 
   scanf(" %s", input); 
-  unsigned int* number_pt = (unsigned int*) &(input); 
-  unsigned int number = *(number_pt);
   for (int i = 3; i >= 0; i--) { 
-    printf("Debug: %c = 0x%08X \n", input[i], input[i] << i*8);
+    current = input[i] << ((3-i)*8);
+    number += current; 
+    printf("Debug: %c = 0x%08X \n", input[i], input[i] << ((3-i)*8));
   }
   printf("Your number is %i (0x%08X).\n", number, number); 
   return 0;
