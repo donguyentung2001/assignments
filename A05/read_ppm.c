3edb6621fabaa58f5e6b0170a5dc12e648cdb1be
Tung Do <tdo@haverford.edu>
2022-02-22 23:12:17 -0500

    done 3

--- A05/read_ppm.c
+++ A05/read_ppm.c
@@ -7,8 +7,45 @@
 // Feel free to change the function signature if you prefer to implement an 
 // array of arrays
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
+  char buffer[1024]; 
+  char current[1024]; 
+  int row; 
+  int col; 
+  struct ppm_pixel current_pixel; 
 
-  return NULL;
+  FILE *fp = fopen(filename, "rb"); 
+  if (!fp) { 
+    printf("Unable to open file. \n"); 
+    return NULL; 
+  }
+  fgets(buffer, 1024, fp); 
+  sscanf(buffer, "%s", current); 
+  fgets(buffer, 1024, fp); 
+  printf("%s \n", current); 
+  sscanf(buffer, "%s", current); 
+
+  while(current[0] == '#') { 
+    printf("Spotting and skipping comments. \n"); 
+    fgets(buffer, 1024, fp); 
+    sscanf(buffer, "%s", current); 
+  }
+  sscanf(buffer, "%i %i", &row, &col); 
+  *w = row; 
+  *h = col; 
+  fgets(buffer, 1024, fp); 
+
+  struct ppm_pixel* ppm_list = malloc(sizeof(struct ppm_pixel)*(*w)*(*h));
+  if (ppm_list == NULL) { 
+    printf("Cannot allocate memory. \n");
+    return NULL; 
+  }
+  for (int i = 0; i < (*w)*(*h); i++) { 
+    fread(&current_pixel, 3, 1, fp); 
+    ppm_list[i] = current_pixel; 
+  }
+
+  fclose(fp); 
+  return ppm_list;
 }
 
 // TODO: Implement this function
--- A05/test_ppm.c
+++ A05/test_ppm.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h> 
 #include "read_ppm.h"
 
 int main(int argc, char** argv) {
+  struct ppm_pixel* pixel_pointer; 
+  struct ppm_pixel current_pixel; 
+  // todo: call read_ppm
+  int w;
+  int h; 
+  pixel_pointer = read_ppm("feep-raw.ppm", &w, &h); 
+  printf("Testing file feep-raw.ppm: %i %i. \n", w, h); 
+  for (int i = 0; i < h; i++) { 
+    for (int j = 0; j < w; j++) { 
+      current_pixel = pixel_pointer[i*w + j]; 
+      printf("(%hhu, %hhu, %hhu) ", current_pixel.red, current_pixel.green, current_pixel.blue); 
+    }
+    printf("\n");
+  }
 
+  free(pixel_pointer); 
+  pixel_pointer = NULL; 
   return 0;
 }
 
