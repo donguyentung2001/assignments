24503a583e3008ad6015d97ff104ae7093702efd
Tung Do <tdo@haverford.edu>
2022-02-27 21:48:06 -0500

    done 1

--- A06/decode.c
+++ A06/decode.c
@@ -1,7 +1,66 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "read_ppm.h"
 
 int main(int argc, char** argv) {
+  unsigned int result; 
+  if (argc != 2) { 
+    printf("Need two arguments. \n"); 
+    exit(1); 
+  }
+  struct ppm_pixel* pixel_pointer; 
+  struct ppm_pixel current_pixel; 
+
+  // todo: call read_ppm
+  int w;
+  int h; 
+  int count = 0; 
+  int index = 0;
+  char binary_char[9]; 
+  
+  unsigned int maskLeast = 0x0001;
+  pixel_pointer = read_ppm(argv[1], &w, &h); 
+  printf("Testing file %s with width %i and height %i. \n", argv[1], w, h); 
+
+  char* output = malloc(sizeof(char)*(((w*h*3)/8)+1)); 
+
+  if (!output) { 
+    printf("Cannot allocate memory. \n"); 
+    exit(1); 
+  }
+
+  for (int i = 0; i < h; i++) { 
+    for (int j = 0; j < w; j++) { 
+      current_pixel = pixel_pointer[i*w + j];
+      result = current_pixel.red & maskLeast;  
+      for (int k = 0; k < 3; k++) { 
+        result = current_pixel.colors[k] & maskLeast;  
+        if (result == 0) {
+          binary_char[count] = '0'; 
+        }
+        else { 
+          binary_char[count] = '1'; 
+        }
+        count += 1; 
+        if (count == 8) { 
+          binary_char[8] = '\0'; 
+          char c = strtol(binary_char, 0, 2);
+          output[index] = c; 
+          count = 0; 
+          index += 1; 
+        }
+      } 
+    }
+  }
+  
+  output[(w*h*3)/8+1] = '\0'; 
+  printf("Max number of characters in the image: %i. \n", (w*h*3)/8);
+  printf("%s \n", output); 
+  free(pixel_pointer); 
+  pixel_pointer = NULL; 
+  free(output); 
+  output = NULL; 
+
   return 0;
 }
 
--- A06/read_ppm.c
+++ A06/read_ppm.c
@@ -1,19 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "read_ppm.h"
 
 // TODO: Implement this function
 // Feel free to change the function signature if you prefer to implement an 
 // array of arrays
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
+  char buffer[1024]; 
+  char current[1024]; 
+  int row; 
+  int col; 
+  struct ppm_pixel current_pixel; 
 
-  return NULL;
+  FILE *fp = fopen(filename, "rb"); 
+
+  if (!fp) { 
+    printf("Unable to open file. \n"); 
+    return NULL; 
+  }
+  fgets(buffer, 1024, fp); 
+  sscanf(buffer, "%s", current); 
+  fgets(buffer, 1024, fp); 
+  sscanf(buffer, "%s", current); 
+
+  while(current[0] == '#') { 
+    fgets(buffer, 1024, fp); 
+    sscanf(buffer, "%s", current); 
+  }
+  sscanf(buffer, "%i %i", &row, &col); 
+  *w = row; 
+  *h = col; 
+  fgets(buffer, 1024, fp); 
+
+  struct ppm_pixel* ppm_list = malloc(sizeof(struct ppm_pixel)*(*w)*(*h));
+  if (ppm_list == NULL) { 
+    printf("Cannot allocate memory. \n");
+    return NULL; 
+  }
+  for (int i = 0; i < (*w)*(*h); i++) { 
+    fread(&current_pixel, 3, 1, fp); 
+    ppm_list[i] = current_pixel; 
+  }
+
+  fclose(fp); 
+  return ppm_list;
 }
 
 // TODO: Implement this function
 // Feel free to change the function signature if you prefer to implement an 
 // array of arrays
 extern void write_ppm(const char* filename, struct ppm_pixel* pxs, int w, int h) {
+  srand(time(NULL));
+  struct ppm_pixel current_pixel;
 
+  FILE* outputFile = fopen(filename, "wb"); 
+
+  if (!outputFile) { 
+    printf("cannot create file. \n"); 
+    exit(1); 
+  }
+
+  fprintf(outputFile, "%s\n%d %d\n%d\n", "P6", w, h, 255);
+  for (int i = 0; i < (w)*(h); i++) { 
+    current_pixel = pxs[i]; 
+    current_pixel.red = current_pixel.red << (rand() % 5); 
+    current_pixel.green = current_pixel.green << (rand() % 5); 
+    current_pixel.blue = current_pixel.blue << (rand() % 5); 
+    fwrite(&current_pixel, 3, 1, outputFile); 
+  }
+  
+  fclose(outputFile);
 }
