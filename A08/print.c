7bf953a982bb5f1ce1afca4f5a867b91df261c7a
Tung Do <tdo@haverford.edu>
2022-03-31 23:26:43 -0400

    print

--- A08/print.c
+++ A08/print.c
@@ -6,8 +6,9 @@
 
 int main (int argc, char* argv[]) {
   void *init = sbrk(0);
-  printf("initial top of heap is %p", init); 
+  printf("initial top of heap is %p. \n", init); 
   void *new = sbrk(0); 
-  printf("new top of heap is %p", new); 
+  printf("new top of heap is %p. \n", new);
+  printf("We have allocated %i bytes", (int) (init-new));  
   return 0 ;
 }
