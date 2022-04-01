a47e2bf3693a94c5720e470e951ddd4da4932be1
Tung Do <tdo@haverford.edu>
2022-04-01 00:49:13 -0400

    reallocate

--- A08/print.c
+++ A08/print.c
@@ -9,6 +9,6 @@ int main (int argc, char* argv[]) {
   printf("initial top of heap is %p. \n", init); 
   void *new = sbrk(0); 
   printf("new top of heap is %p. \n", new);
-  printf("We have allocated %i bytes", (int) (init-new));  
+  printf("We have allocated %i bytes. \n", (int) (new - init));  
   return 0 ;
 }
--- A08/reallocate.c
+++ A08/reallocate.c
@@ -5,6 +5,26 @@
 #include "rand.h"
 
 int main (int argc, char* argv[]) {
+  void *init; 
+  void *final; 
+  for (int i = 0; i < 10; i++) { 
+    void *alloc_memory = malloc(100); 
+    if (alloc_memory == NULL) { 
+      printf("Fail to allocate memory. \n"); 
+      exit(1); 
+    }
+    if (i==0) {
+      init = alloc_memory; 
+    }
+    if (i == 9) { 
+      final = alloc_memory; 
+    }
+    free(malloc); 
+    alloc_memory = NULL; 
+  }
+  printf("initial top of heap is %p. \n", init); 
+  printf("New top of heap is %p. \n", final);
+  printf("We have allocated %i bytes. \n", (int) (final - init)); 
 
   return 0 ;
 }
