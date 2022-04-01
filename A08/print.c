863ebc78da901770ec63d985559b24e34667a428
Tung Do <tdo@haverford.edu>
2022-04-01 16:38:45 -0400

    done

--- A08/print.c
+++ A08/print.c
@@ -4,11 +4,17 @@
 #include <time.h>
 #include "rand.h"
 
+struct chunk {
+  int size;
+  int memory_used; 
+  struct chunk *next;
+};
 int main (int argc, char* argv[]) {
   void *init = sbrk(0);
   printf("initial top of heap is %p. \n", init); 
   void *new = sbrk(0); 
   printf("new top of heap is %p. \n", new);
   printf("We have allocated %i bytes. \n", (int) (new - init));  
+  printf("size of chunk %i. \n", sizeof(struct chunk*))
   return 0 ;
 }
