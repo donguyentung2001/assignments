1a422165286ac2b07a56730c0c1256f27b8dd8d4
Tung Do <tdo@haverford.edu>
2022-04-22 10:52:28 -0400

    finish 1

--- A10/read_ppm.c
+++ A10/read_ppm.c
@@ -1,16 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "read_ppm.h"
 
+// TODO: Implement this function
+// Feel free to change the function signature if you prefer to implement an 
+// array of arrays
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
+  char buffer[1024]; 
+  char current[1024]; 
+  int row; 
+  int col; 
+  struct ppm_pixel current_pixel; 
 
-  // your code here
-  return NULL;
-}
+  FILE *fp = fopen(filename, "rb"); 
 
-void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
+  if (!fp) { 
+    printf("Unable to open file. \n"); 
+    return NULL; 
+  }
+  fgets(buffer, 1024, fp); 
+  sscanf(buffer, "%s", current); 
+  fgets(buffer, 1024, fp); 
+  sscanf(buffer, "%s", current); 
 
-  // your code here
+  while(current[0] == '#') { 
+    fgets(buffer, 1024, fp); 
+    sscanf(buffer, "%s", current); 
+  } 
+  sscanf(buffer, "%i %i", &row, &col); 
+  *w = row; 
+  *h = col; 
+  fgets(buffer, 1024, fp); 
+
+  struct ppm_pixel* ppm_list = malloc(sizeof(struct ppm_pixel)*(*w)*(*h)); 
+  if (ppm_list == NULL) { 
+    printf("Cannot allocate memory. \n"); 
+    return NULL; 
+  } 
+  for (int i = 0; i < (*w)*(*h); i++) { 
+    fread(&current_pixel, 3, 1, fp); 
+    ppm_list[i] = current_pixel; 
+  }
+
+  fclose(fp); 
+  return ppm_list; 
+} 
+
+// TODO: Implement this function
+// Feel free to change the function signature if you prefer to implement an 
+// array of arrays
+extern void write_ppm(const char* filename, struct ppm_pixel* pxs, int w, int h) {
+  srand(time(NULL));
+  struct ppm_pixel current_pixel;
+
+  FILE* outputFile = fopen(filename, "wb"); 
+
+  if (!outputFile) { 
+    printf("cannot create file. \n"); 
+    exit(1); 
+  }
+
+  fprintf(outputFile, "%s\n%d %d\n%d\n", "P6", w, h, 255);
+  for (int i = 0; i < (w)*(h); i++) { 
+    current_pixel = pxs[i]; 
+    fwrite(&current_pixel, 3, 1, outputFile); 
+  }
+  
+  fclose(outputFile);
+
 }
 
--- A10/thread_mandelbrot.c
+++ A10/thread_mandelbrot.c
@@ -5,7 +5,67 @@
 #include <time.h>
 #include <pthread.h>
 #include "read_ppm.h"
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
 
+struct image_data { 
+  int id; 
+  int size; 
+  int col_start; 
+  int col_end; 
+  int row_start; 
+  int row_end; 
+  struct ppm_pixel* image; 
+  struct ppm_pixel* palette;
+}; 
+
+void* compute_image(void *args) { 
+  float xmin = -2.0;
+  float xmax = 0.47;
+  float ymin = -1.12;
+  float ymax = 1.12;
+  int maxIterations = 1000;
+  struct ppm_pixel image_color;
+  struct image_data *data = (struct image_data *) args;
+  int col_start = data->col_start; 
+  int row_start = data->row_start; 
+  int col_end = data->col_end; 
+  int row_end = data->row_end; 
+  printf("Thread %i is computing subimage block with row(%i, %i) and col(%i,%i). \n", data->id, row_start, row_end, col_start, col_end);
+  for (int col = col_start; col < col_end; col++) { 
+      for (int row = row_start; row < row_end; row++) { 
+        float xfrac = (float)row/ (float)data->size; 
+        float yfrac = (float) col/(float)data->size; 
+        float x0 = xmin + xfrac * (xmax - xmin); 
+        float y0 = ymin + yfrac * (ymax - ymin); 
+    
+        float x = 0; 
+        float y = 0; 
+        int iter = 0; 
+        while (iter < maxIterations && (x*x + y*y < 2*2)) { 
+          float xtmp = x*x - y*y + x0; 
+          y = 2*x*y + y0; 
+          x = xtmp; 
+          iter++; 
+        }
+        if (iter < maxIterations) { 
+          image_color = data->palette[iter]; 
+        }
+        else { 
+          image_color.red = 0; 
+          image_color.green = 0; 
+          image_color.blue = 0; 
+        }
+        data->image[col*data->size + row] = image_color; 
+      }
+    }
+    printf("Thread %i finished. \n", data->id); 
+    return (void*) NULL;
+}
 int main(int argc, char* argv[]) {
   int size = 480;
   float xmin = -2.0;
@@ -13,7 +73,7 @@ int main(int argc, char* argv[]) {
   float ymin = -1.12;
   float ymax = 1.12;
   int maxIterations = 1000;
-  int numProcesses = 4;
+  int numThreads = 4;
 
   int opt;
   while ((opt = getopt(argc, argv, ":s:l:r:t:b:p:")) != -1) {
@@ -28,11 +88,98 @@ int main(int argc, char* argv[]) {
     }
   }
   printf("Generating mandelbrot with size %dx%d\n", size, size);
-  printf("  Num processes = %d\n", numProcesses);
+  printf("  Num threads = %d\n", numThreads);
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
   // todo: your code here
   // generate pallet
+  struct ppm_pixel* palette = malloc(sizeof(struct ppm_pixel)*1000);
+  if (palette == NULL) { 
+    printf("Cannot allocate memory"); 
+    exit(1); 
+  } 
+  struct ppm_pixel current_color;
+  srand(time(0));
+  for (int i=0; i<maxIterations; i++) { 
+    current_color.red = rand() % 255; 
+    current_color.green = rand() % 255; 
+    current_color.blue = rand() % 255;
+    palette[i] = current_color;
+  }
   // compute image
+  struct timeval tstart, tend;
+  struct image_data images_data[4]; 
+  struct ppm_pixel* image = malloc(sizeof(struct ppm_pixel)*size*size); 
+  if (image == NULL) {
+    printf("Unable to allocate memory"); 
+    exit(1);
+  }
+  pthread_t threads[4];
+  double timer; 
+  gettimeofday(&tstart, NULL);
+  for (int i = 0; i <4; i++) { 
+    int col_start;
+    int row_start;
+    int col_end; 
+    int row_end;  
+    if (i == 0) { 
+      col_start = 0; 
+      row_start = 0; 
+      col_end = size/2;
+      row_end = size/2;
+    }
+    if (i == 1) { 
+      col_start = size/2; 
+      row_start = 0; 
+      col_end = size;
+      row_end = size/2;
+    }
+    if (i == 2) { 
+      col_start = 0; 
+      row_start = size/2; 
+      col_end = size/2;
+      row_end = size;
+    }
+    if (i == 3) { 
+      col_start = size/2; 
+      row_start = size/2; 
+      col_end = size;
+      row_end = size;
+    }
+    images_data[i].size = size; 
+    images_data[i].col_start = col_start; 
+    images_data[i].row_start = row_start; 
+    images_data[i].col_end = col_end; 
+    images_data[i].row_end = row_end; 
+    images_data[i].palette = palette;
+    images_data[i].image = image;
+    images_data[i].id = i+1; 
+    pthread_create(&threads[i], NULL, compute_image, (void*) &images_data[i]); 
+  }
+    for (int i = 0; i < 4; i++) {
+      pthread_join(threads[i], NULL);
+    }
+
+    gettimeofday(&tend, NULL);
+    timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
+    printf("Compute mandelbrot set of size (%i, %i) in %f second(s). \n", size, size, timer); 
+    char output_filename[100]; 
+    char size_char[8];
+    char time_char[50]; 
+    time_t second = time(0); 
+    sprintf(size_char, "%d", size); 
+    sprintf(time_char, "%ld", second); 
+    strcpy(output_filename, "mandelbrot-"); 
+    strcat(output_filename, size_char); 
+    strcat(output_filename, "-"); 
+    strcat(output_filename, time_char); 
+    strcat(output_filename,".ppm"); 
+    printf("Writing file %s. \n", output_filename);
+    write_ppm(output_filename, image, size, size); 
+    //free memory 
+    free(image); 
+    image = NULL; 
+    free(palette);
+    palette = NULL;
 }
