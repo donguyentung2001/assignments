2e02a7a9fdeaca34dc122b58b29e848bf8e58f0b
Tung Do <tdo@haverford.edu>
2022-04-22 14:39:56 -0400

    more debug

--- A10/buddhabrot.c
+++ A10/buddhabrot.c
@@ -174,7 +174,7 @@ int main(int argc, char* argv[]) {
   double timer; 
   gettimeofday(&tstart, NULL);
   pthread_mutex_init(&mutex, NULL);
-  pthread_barrier_init(&barrier, NULL);
+  pthread_barrier_init(&barrier, NULL, 4);
   for (int i = 0; i <4; i++) { 
     int col_start;
     int row_start;
